79b2f6fc7579d9103e6fd9d373871b0757c64ea5
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-09-14 15:31:54 +0530

    Provided probability

--- test/com/tw/step8/toss/CoinsTest.java
+++ test/com/tw/step8/toss/CoinsTest.java
@@ -8,28 +8,28 @@ class CoinsTest {
 
   @Test
   void ShouldGiveChanceOfGettingTails() {
-    Coins coins = new Coins(1);
+    Coins coins = new Coins(0.5, 1);
     assertEquals(0.5, coins.chanceOfAllTails());
 
-    Coins coins2 = new Coins(2);
+    Coins coins2 = new Coins(0.5, 2);
     assertEquals(0.25, coins2.chanceOfAllTails());
   }
 
   @Test
   void ShouldGiveChanceOfNoTails() {
-    Coins coins = new Coins(1);
+    Coins coins = new Coins(0.5, 1);
     assertEquals(0.5, coins.chanceOfNoTails());
 
-    Coins coins2 = new Coins(2);
+    Coins coins2 = new Coins(0.5, 2);
     assertEquals(0.25, coins2.chanceOfNoTails());
   }
 
   @Test
   void ShouldGiveChanceOfGettingAtLeastOneTails() {
-    Coins coins = new Coins(2);
+    Coins coins = new Coins(0.5, 2);
     assertEquals(0.75, coins.chanceOfAtLeastOneTails());
 
-    Coins coins2 = new Coins(3);
+    Coins coins2 = new Coins(0.5, 3);
     assertEquals(0.875, coins2.chanceOfAtLeastOneTails());
   }
 }
