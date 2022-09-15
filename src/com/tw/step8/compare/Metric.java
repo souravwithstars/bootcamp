a9cd49050334c7bc84b4f32d704ed38727a1b75a
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-09-15 11:59:02 +0530

    Removed unnecessary properties from Metric class

--- src/com/tw/step8/compare/Metric.java
+++ src/com/tw/step8/compare/Metric.java
@@ -1,14 +1,10 @@
 package com.tw.step8.compare;
 
 public class Metric {
-  private final double higherMetric;
-  private final double lowerMetric;
   private final double factor;
 
   private Metric(double higherMetric, double lowerMetric) {
-    this.higherMetric = higherMetric;
-    this.lowerMetric = lowerMetric;
-    this.factor = this.lowerMetric / this.higherMetric;
+    this.factor = lowerMetric / higherMetric;
   }
 
   public static Metric setMetrics(double higherMetric, double lowerMetric) {
